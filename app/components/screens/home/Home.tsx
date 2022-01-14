c9563e3d6073274663da650ff524960a1e3bf5fa
dimma111 <marinenko96@yandex.ru>
2022-01-14 17:26:06 +0500

    Add layout and components

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../../hooks/useAuth";
+import Layout from "../../layouts/Layout";
+import Header from "./Header";
+import Stories from "./stories/Stories";
 
 export default function Home() {
+  const { logout, user } = useAuth();
+
+  // logout();
   return (
-    <View>
+    <Layout>
+      <Header />
+      <Stories />
       <Text>HOME SCREEN</Text>
-    </View>
+    </Layout>
   );
 }
