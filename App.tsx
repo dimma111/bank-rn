ba00f6c61bb555f8a5eb39a6a44ad51a11583a6d
dimma111 <marinenko96@yandex.ru>
2022-01-14 17:21:12 +0500

    Add data provider

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { LogBox, StyleSheet, Text, View } from "react-native";
 import { AuthProvider } from "./app/providers/AuthProvider";
 import Navigation from "./app/navigation/Navigation";
+import { DataProvider } from "./app/providers/DataProvider";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Navigation />
+      <DataProvider>
+        <Navigation />
+      </DataProvider>
     </AuthProvider>
   );
 }
